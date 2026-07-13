6b4fe8246b12200bcdcb0a5de265b6f382a753e2
zhuang-zeng-guang <1979191609@qq.com>
2026-07-13 08:46:57 +0800

    修复GitHub Pages路由问题

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 // createRouter:创建router实例对象
 // createWebHistory:创建history模式的路由
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Layout from '@/views/Layout/index.vue'
 import Login from '@/views/Login/index.vue'
 import Home from '@/views/Home/index.vue'
@@ -17,7 +17,7 @@ import UserOrder from '@/views/Member/components/UserOrder.vue'
 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   // path和component对应关系的位置
   routes: [
     {
